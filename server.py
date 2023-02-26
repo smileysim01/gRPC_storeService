0c23ae36694c24627e25f93e48069bee00f92d38
Rishabh Gundavarapu <rgundavarpu@wisc.edu>
2023-02-26 06:12:23 +0000

    should recheck part 1

---
+++ server.py
@@ -0,0 +1,17 @@
+import grpc
+from concurrent import futures
+import numstore_pb2_grpc
+import numstore_pb2
+
+Class meow(numstore_pb2_grpc.NumStoreServicer):
+    def SetNum(self,request,context):
+        print(request)
+        return numstore_pb2.SetNumResp(result=3)
+       
+
+server = grpc.server(futures.ThreadPoolExecutor(max_workers=4), options=[("grpc.so_reuseport", 0)])
+numstore_pb2_grpc.add_NumStore_to_server(meow(),server())
+print('LISTENING ON PORT : 5444')
+server.add_insecure_port('localhost:5444')
+server.start()
+server.wait_for_termination()
