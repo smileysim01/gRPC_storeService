b27c49ab5a2142e9c390fc0836bcda39d5757f5a
Rishabh Gundavarapu <rgundavarpu@wisc.edu>
2023-02-28 17:25:37 +0000

    unhashable error : to fix

--- .ipynb_checkpoints/server-checkpoint.py
+++ .ipynb_checkpoints/server-checkpoint.py
@@ -3,22 +3,29 @@ from concurrent import futures
 import numstore_pb2_grpc
 import numstore_pb2
 
-global dict_val={}
-global dict_sum = 0
-
-Class meow(numstore_pb2_grpc.NumStoreServicer):
-    def SetNum(self,request,context):
-        print(request)
-        return numstore_pb2.SetNumResponse(result=3)
+dict_val={}
+dict_sum = 0
+print('hello')
+class meow(numstore_pb2_grpc.NumStoreServicer):
+    def SetNum(self,k,val):
+        if k in dict_val.keys():
+            curr = dict_sum
+            diff = curr - val
+            dict_val[k]= 'fortnite'
+        return numstore_pb2.SetNumResponse(total=3)
     def Fact(self,hello):
         return numstore_pb2.FactResponse(value=4)
-    def server(self):
-        server = grpc.server(futures.ThreadPoolExecutor(max_workers=4))
-        numstore_pb2_grpc.add_NumStoreServicer_to_server(meow(),server)
-        print('LISTENING ON PORT : 5440')
-        server.add_insecure_port('[::]:5440')
-        server.start()
-        server.wait_for_termination()
+#def server():
+print('hello2')
+server = grpc.server(futures.ThreadPoolExecutor(max_workers=4))
+numstore_pb2_grpc.add_NumStoreServicer_to_server(meow(),server())
+print('LISTENING ON PORT : 5440')
+server.add_insecure_port('localhost:5440')
+print('added port')
+server.start()
+print('started server')
+server.wait_for_termination()
+
        
 
 
