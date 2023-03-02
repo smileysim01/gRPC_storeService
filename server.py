ca2be8616a89b8b1e2b41aa7fe3a981f28d24c9f
Rishabh Gundavarapu <rgundavarpu@wisc.edu>
2023-03-02 03:29:31 +0000

    new changes

--- server.py
+++ server.py
@@ -1,13 +1,15 @@
+#rgundavarapu@wisc.edu
 import grpc
 from concurrent import futures
 import numstore_pb2_grpc
 import numstore_pb2
 import threading
-
 mylock=threading.Lock()
 
-dict_val={}
-dict_sum = 0
+my_cache=[] # implementing LRU
+cache_size=10
+dict_val={} # dictionary of values
+dict_sum = 0 # sum of values in the dictionary
 class meow(numstore_pb2_grpc.NumStoreServicer):
     print('meow')
     def SetNum(self,k,val):
@@ -17,27 +19,20 @@ class meow(numstore_pb2_grpc.NumStoreServicer):
             print('inside lock')
             if k.key in dict_val.keys():
                 curr=dict_val[k.key]
-                dict_val[k.key]=k.value
-                dict_sum+=k.value-curr
+                dict_val[k.key]=k.value # update the value
+                dict_sum+=(k.value-curr) # update the total by calculating the diff between old and new value
             else:
                 dict_val[k.key]=k.value
                 dict_sum+=k.value
-            #if k in (dict_val.keys()):
-               # print('meow')
-            #     print('inside keys')
-            #     curr = dict_sum
-            #     diff = curr - val
-            #     dict_val[k]= 'fortnite'
             print('about to return setnum')
         return numstore_pb2.SetNumResponse(total=dict_sum)
     def Fact(self,factkey,factval):
         factorial=1
-        print('inside fact')
+        #print('inside fact')
         if factkey.key in dict_val.keys():
-            print('inside if')
-            print(factkey.key)
+            #print('inside if')
+            #print(factkey.key)
             for i in range(1,dict_val[factkey.key]+1):
-                print('inside loop')
                 factorial*=i
             return numstore_pb2.FactResponse(value=factorial)
         else:
