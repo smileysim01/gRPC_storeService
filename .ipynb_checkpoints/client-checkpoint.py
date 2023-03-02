bcc238a3fb994c059a2f708cf2ec12483a0df40c
Rishabh Gundavarapu <rgundavarpu@wisc.edu>
2023-03-02 04:44:58 +0000

    part 2 done

---
+++ .ipynb_checkpoints/client-checkpoint.py
@@ -0,0 +1,12 @@
+#rgundavarapu@wisc.edu
+import grpc
+from concurrent import futures
+import numstore_pb2_grpc
+import numstore_pb2
+import threading
+import collections
+import numpy as np
+
+thread_count=8 # 8 or 4 ????
+port=sys.argv[1] # get port submitted when running this client
+class my_client(threading.Thread):
